eb6b3d65fbce59cfe0b819a8be5b18df2266a06a
kimjooeun <ducky_313@naver.com>
2022-10-24 11:47:21 +0900

    Feat: create kkalong springboot project

---
+++ Kkalong_BE/src/main/java/com/ssafy/kkalong/KkalongApplication.java
@@ -0,0 +1,14 @@
+package com.ssafy.kkalong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkalongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkalongApplication.class, args);
+        System.out.println("hello kkalong world");
+    }
+
+}
