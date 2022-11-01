413428a5b820a8c00de71c32ebe67498e2266441
kimjooeun <ducky_313@naver.com>
2022-11-01 09:53:27 +0900

    [BE - S07P31B302-328] FEAT : controller 매핑 설정

--- Kkalong_BE/src/main/java/com/ssafy/kkalong/api/controller/UserController.java
+++ Kkalong_BE/src/main/java/com/ssafy/kkalong/api/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.ssafy.kkalong.api.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/user")
 public class UserController {
 }
