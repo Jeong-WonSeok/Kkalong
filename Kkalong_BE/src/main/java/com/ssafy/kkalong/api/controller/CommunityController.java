097e9b42809e512a7fa051b6f065a7b99d1430bb
Jeong-Wonseok <jws4858@gmail.com>
2022-11-02 08:03:48 +0900

    [BE - S07P31B302-306] FEAT : community upload

--- Kkalong_BE/src/main/java/com/ssafy/kkalong/api/controller/CommunityController.java
+++ Kkalong_BE/src/main/java/com/ssafy/kkalong/api/controller/CommunityController.java
@@ -1,9 +1,32 @@
 package com.ssafy.kkalong.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ssafy.kkalong.api.service.CommunityService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
+@RequiredArgsConstructor
 @RequestMapping("/community")
 public class CommunityController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final CommunityService communityService;
+
+//    @GetMapping(value="best")
+
+    @GetMapping(value="bestdress")
+    public ResponseEntity<?> bestDress(){
+
+        communityService.Post
+
+        return ResponseEntity.ok().body('a');
+    }
+
+//    @PostMapping(value="bestdress")
+
 }
--- Kkalong_BE/src/main/java/com/ssafy/kkalong/api/service/CommunityService.java
+++ Kkalong_BE/src/main/java/com/ssafy/kkalong/api/service/CommunityService.java
@@ -1,7 +1,20 @@
 package com.ssafy.kkalong.api.service;
 
+import com.ssafy.kkalong.api.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class CommunityService {
+
+    private final PostRepository postRepository;
+
+    public List<?> selectBestPost(){
+        List<?> findByBestPost = postRepository.findByBestPost();
+
+        return findByBestPost;
+    }
 }
