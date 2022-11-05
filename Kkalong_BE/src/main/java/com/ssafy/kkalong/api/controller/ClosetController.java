13e7b53bbd6296e005d9885fadae512c9bbe2c7f
kimjooeun <ducky_313@naver.com>
2022-11-05 14:32:33 +0900

    [BE - S07P31B302-313] FEAT : closet controller 작업

--- Kkalong_BE/src/main/java/com/ssafy/kkalong/api/controller/ClosetController.java
+++ Kkalong_BE/src/main/java/com/ssafy/kkalong/api/controller/ClosetController.java
@@ -1,9 +1,13 @@
 package com.ssafy.kkalong.api.controller;
 
 import com.ssafy.kkalong.api.dto.BestDressResponseInterface;
+import com.ssafy.kkalong.api.dto.ClosetInfoDto;
 import com.ssafy.kkalong.api.entity.Closet;
 import com.ssafy.kkalong.api.entity.User;
+import com.ssafy.kkalong.api.service.ClosetService;
+import com.ssafy.kkalong.api.service.CommunityService;
 import com.ssafy.kkalong.security.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,26 +25,16 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RequestMapping("/closet")
 public class ClosetController {
 
+    @Autowired
+    ClosetService closetService;
+
     @GetMapping("/all")
     public ResponseEntity<?> getAllClosetByUserId(@AuthenticationPrincipal UserDetailsImpl userInfo){
         List<Map<String, Object>> result = new ArrayList<>();
-        List<Closet> closets = userService.
+//        List<Closet> closets = closetService.getClosetsByUserId(userInfo.getId());
+//        for (Closet closet : closets) {
 
-        for (BestDressResponseInterface bd : post) {
-            System.out.println(bd.getId());
-            Map<String, Object> temp = new HashMap<>();
-            Map<String, Object> temp_u = new HashMap<>();
-            User user = communityService.selectUser(bd.getId());
-
-            String nick = user.getNickname();
-            String profile_img = user.getImg();
-
-            temp.put("Best", bd);
-            temp_u.put("NickName", nick);
-            temp_u.put("profile_img", profile_img);
-            temp.put("User", temp_u);
-            result.add(temp);
-        }
+//        }
 
         return ResponseEntity.ok().body(result);
     }
