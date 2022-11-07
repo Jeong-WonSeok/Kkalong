f1ea457751106ed43259b2c84a58c3358f7ab9eb
tmddn1008 <tmddn1008@naver.com>
2022-11-07 17:42:52 +0900

    [FE - S07P31B302-398] FEAT : three.js 기능

--- kkalong_fe/src/components/closet/Example.js
+++ kkalong_fe/src/components/closet/Example.js
@@ -1,18 +1,48 @@
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import styled from "styled-components";
-
 import Model from "./Model";
+import { useNavigate } from "react-router-dom";
 
 const Example = () => {
+
+  const navigate = useNavigate();
+  
+  const updateHandler = (e) => {
+    console.log("업뎃핸들러")
+    setTimeout(()=> {
+      setTimeout(()=> {
+        navigate('/VirtualFitting/VirtualBrandChoice/')
+      },2500)
+      for(let i=0; i<110; i++) {
+        setTimeout(()=> {
+          e.scale.z += i *0.003;
+        }, i * 50)
+      }
+    },2500)
+  }
+
+
+
   return (
     <>
       <Contain>
         <Canvas>
+
           <Suspense fallback={null}>
+           
             <directionalLight intensity={1} />
-            <ambientLight intensity={1.2} />
+            <ambientLight intensity={1.0} />
+            <PerspectiveCamera onUpdate={updateHandler}
+                        makeDefault={true}
+                        far={300}
+                        near={0.1}
+                        fov={70.1}
+                        position={[0,0,6]}
+                        zoom={1}
+                        
+                    />
             <spotLight
               intensity={0.1}
               angle={0.1}
@@ -20,7 +50,11 @@ const Example = () => {
               position={[10, 15, 10]}
               castShadow
             />
-            <Model />
+            <Model 
+              position={[0, -2, -1]}
+              rotation-y={Math.PI * 1.5}
+             
+            />
             <OrbitControls
               enablePan={true}
               enableZoom={true}
@@ -39,5 +73,5 @@ const Contain = styled.div`
   width: 100%;
   height: 600px;
   margin: 0 auto;
-  background: #2d2d2d;
+  background: #FFFFFF;
 `;
