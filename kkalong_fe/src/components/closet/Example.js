e3bf86506cf20655b705be5cdfb3d6141ffb071e
김호진 <hjay1029@naver.com>
2022-11-08 17:46:13 +0900

    [FE - S07P31B302-403 ] FEAT: 캔버스 이미지 업로드 수정

--- kkalong_fe/src/components/closet/Example.js
+++ kkalong_fe/src/components/closet/Example.js
@@ -6,43 +6,38 @@ import Model from "./Model";
 import { useNavigate } from "react-router-dom";
 
 const Example = () => {
-
   const navigate = useNavigate();
-  
+
   const updateHandler = (e) => {
-    console.log("업뎃핸들러")
-    setTimeout(()=> {
-      setTimeout(()=> {
-        navigate('/VirtualFitting/VirtualBrandChoice/')
-      },2500)
-      for(let i=0; i<110; i++) {
-        setTimeout(()=> {
-          e.scale.z += i *0.003;
-        }, i * 50)
+    console.log("업뎃핸들러");
+    setTimeout(() => {
+      setTimeout(() => {
+        navigate("/VirtualFitting/VirtualBrandChoice/");
+      }, 2500);
+      for (let i = 0; i < 110; i++) {
+        setTimeout(() => {
+          e.scale.z += i * 0.003;
+        }, i * 50);
       }
-    },2500)
-  }
-
-
+    }, 2500);
+  };
 
   return (
     <>
       <Contain>
         <Canvas>
-
           <Suspense fallback={null}>
-           
             <directionalLight intensity={1} />
             <ambientLight intensity={1.0} />
-            <PerspectiveCamera onUpdate={updateHandler}
-                        makeDefault={true}
-                        far={300}
-                        near={0.1}
-                        fov={70.1}
-                        position={[0,0,6]}
-                        zoom={1}
-                        
-                    />
+            <PerspectiveCamera
+              onUpdate={updateHandler}
+              makeDefault={true}
+              far={300}
+              near={0.1}
+              fov={70.1}
+              position={[0, 0, 6]}
+              zoom={1}
+            />
             <spotLight
               intensity={0.1}
               angle={0.1}
@@ -50,10 +45,11 @@ const Example = () => {
               position={[10, 15, 10]}
               castShadow
             />
-            <Model 
+            <pointLight position={[10, 10, 10]} />
+            <Model
               position={[0, -2, -1]}
               rotation-y={Math.PI * 1.5}
-             
+              color="hotpink"
             />
             <OrbitControls
               enablePan={true}
@@ -73,5 +69,5 @@ const Contain = styled.div`
   width: 100%;
   height: 600px;
   margin: 0 auto;
-  background: #FFFFFF;
+  background: #ffffff;
 `;
