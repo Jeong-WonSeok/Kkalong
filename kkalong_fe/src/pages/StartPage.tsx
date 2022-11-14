c3a8b4e3cda494569bf6af60a1825cd7c088673f
Yenno <csh5156@gmail.com>
2022-11-14 15:30:16 +0900

    [FE - S07P31B302-447] FEAT: 로그인 페이지 디자인 수정

--- kkalong_fe/src/pages/StartPage.tsx
+++ kkalong_fe/src/pages/StartPage.tsx
@@ -1,101 +1,161 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styled, { keyframes } from 'styled-components'
 
-import Kkalong from '../assets/icon/logo/KkalongStart.png'
-import SunGlasses from '../assets/icon/logo/SunGlasses.png'
+import KakaoLoginLogo from "../assets/icon/Login/SocialLogin/kakao.png";
+import GoogleLoginLogo from "../assets/icon/Login/SocialLogin/google.png";
+import KakaoBtn from "../assets/icon/Login/SocialLogin/kakaoBtn.png"
+import GoogleBtn from "../assets/icon/Login/SocialLogin/googleBtn.png"
 
 export default function StartPage() {
   const navigate = useNavigate()
+  const KeyWord = ['나만의 옷장', 'AI', '퍼스널 컬러', '가상피팅']
+  const [idx, setIdx] = useState(0)
   useEffect(()=>{
     const app = document.getElementById('App') as HTMLDivElement
     app.style.margin = '0'
-    setTimeout(()=>{
-      navigate('/login')
-    }, 3500)
-
+    
     return () => {
       app.style.marginTop = '60px'
     }
   }, [])
 
+  useEffect(()=>{
+    setTimeout(()=>{
+      if (idx < 3) {
+        setIdx(idx + 1)
+      } else {
+        setIdx(0)
+      }
+    }, 1290)
+  }, [idx])
+
   return (
     <Container>
       <ImgDiv>
-        <KkalongImg src={Kkalong}/>
-        <SunGlassesImg src={SunGlasses} />
+        <ContentP><PointColor>깔롱</PointColor>은</ContentP>
+        <ContentP># <ChangeP> {KeyWord[idx]}</ChangeP></ContentP> 
       </ImgDiv>
+
+      <LoginContainer>
+        <SocialLoginButton>
+          <SocialLoginLinkTag>
+            <a href="http://k7b302.p.ssafy.io:8080/api/v1/oauth2/authorization/kakao">
+              <SocialImg style={{padding: '0 5px', width: '290px'}} src={KakaoBtn}/>
+            </a>
+          </SocialLoginLinkTag>
+          <SocialLoginLinkTag>
+            <a href="http://k7b302.p.ssafy.io:8080/api/v1/oauth2/authorization/google">
+              <SocialImg src={GoogleBtn}/>
+            </a>
+          </SocialLoginLinkTag>
+        </SocialLoginButton>
+        <RegisterDiv>
+          <RegisterLink onClick={()=>navigate("/signup")}>회원가입</RegisterLink>
+          <RegisterLink onClick={()=>navigate("/login")}>로그인</RegisterLink>
+        </RegisterDiv>
+      </LoginContainer>
     </Container>
   )
 }
 
-const EnterLogo = keyframes`
-  0% {
-    -webkit-filter: blur(12px);
-            filter: blur(12px);
-    opacity: 0;
-  }
-  100% {
-    -webkit-filter: blur(0px);
-            filter: blur(0px);
-    opacity: 1;
-  }
-`
-
-const SunGlassPoint = keyframes`
-  0%,
-  100% {
-    -webkit-transform: translateY(0);
-            transform: translateY(0);
-  }
-  10%,
-  30%,
-  50%,
-  70% {
-    -webkit-transform: translateY(-2px);
-            transform: translateY(-2px);
-  }
-  20%,
-  40%,
-  60% {
-    -webkit-transform: translateY(2px);
-            transform: translateY(2px);
-  }
-  80% {
-    -webkit-transform: translateY(1.6px);
-            transform: translateY(1.6px);
-  }
-  90% {
-    -webkit-transform: translateY(-1.6px);
-            transform: translateY(-1.6px);
-  }
-`
-
 const Container = styled.div`
-  position: fixed;
-  left: auto;
-  top: 0;
-  width: 100%;
   max-width: 360px;
   height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  animation: ${EnterLogo} 1s cubic-bezier(0.550, 0.085, 0.680, 0.530) both;
-`
-
-const KkalongImg = styled.img`
-  width: 200px; 
-`
-
-const SunGlassesImg = styled.img`
-  position: absolute;
-  left: -5px;
-  top: 30px;
-  width: 60px;
-  animation: ${SunGlassPoint} 1.2s cubic-bezier(0.455, 0.030, 0.515, 0.955) 1.5s both;
 `
 
 const ImgDiv = styled.div`
-  position: relative
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin-top: 15px;
 `
+
+const ContentP = styled.p`
+  font-family: var(--main-font-100);
+  font-size: 1.6rem;
+  margin: 0;
+  margin-bottom: 5px;
+`
+
+const PointColor = styled.span`
+  color: var(--primary-color-700)
+`
+
+const titleAnimation = keyframes`
+  0% {
+    color: var(--primary-color-100);
+    transform: translateY(-50px);
+    opacity: 0;
+    -webkit-clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 80%);
+    clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 80%);
+  }
+  20% {
+      color: var(--primary-color-900);
+      transform: translateY(0);
+      opacity: 1;
+      -webkit-clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 15%);
+      clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 15%);
+  }
+  80% {
+      color: var(--primary-color-900);
+      transform: translateY(0);
+      opacity: 1;
+      -webkit-clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 15%);
+      clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 15%);
+  }
+  100% {
+      color: var(--primary-color-100);
+      transform: translateY(50px);
+      opacity: 0;
+      -webkit-clip-path: polygon(100% 0, 100% -0%, 0 100%, 0 100%);
+      clip-path: polygon(100% 0, 100% -0%, 0 100%, 0 100%);
+  };
+`
+
+const ChangeP = styled(ContentP)`
+  display: inline-block;
+  color: var(--primary-color-900);
+  line-height: 2;
+  animation: ${titleAnimation} 1.3s 1.3s infinite;
+` 
+
+const LoginContainer = styled.div`
+  margin-top: 60%;
+`
+
+const SocialLoginButton = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+  width: 300px;
+`;
+
+// 소셜로그인 버튼 태그
+const SocialLoginLinkTag = styled.a``;
+
+const SocialImg = styled.img`
+  width: 300px;
+`
+// 회원가입 관련 div
+const RegisterDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content:space-around;
+  margin-top: 20px;
+`
+
+// 회원가입으로 가는 링크
+const RegisterLink = styled.a`
+  margin: 0px 5px;
+  font-size: 1rem;
+  font-family: var(--base-font-300);
+  text-decoration: none;
+`;
