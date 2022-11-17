530c7d35d41de3fc78ba3909ad996630b697148c
wonseok <jws4858@gmail.com>
2022-11-17 17:49:56 +0900

    [BE - S07P31B302-181] FEAT : personalColor complete
    Former-commit-id: 8f8fe93461c74e415fa94a4e2edac2daf5cd9580

--- Kkalong_BE/src/main/java/com/ssafy/kkalong/api/controller/RecommendController.java
+++ Kkalong_BE/src/main/java/com/ssafy/kkalong/api/controller/RecommendController.java
@@ -1,10 +1,20 @@
 package com.ssafy.kkalong.api.controller;
 
+import com.ssafy.kkalong.api.entity.User;
+import com.ssafy.kkalong.api.service.CommunityService;
 import com.ssafy.kkalong.api.service.RecommendService;
+import com.ssafy.kkalong.api.service.UserService;
+import com.ssafy.kkalong.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin(origins = {"*"})
 @RestController
@@ -12,9 +22,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/recommend")
 public class RecommendController {
 
-    private final RecommendService recommendService;
+    @Autowired
+    RecommendService recommendService;
+    @Autowired
+    UserService userService;
+    @Autowired
+    CommunityService communityService;
 
-//    @GetMapping("/wheater"){
-//
-//    }
+    @PostMapping(consumes = {"multipart/form-data"}, value = "/personal")
+    public ResponseEntity<String> insertPersonal(@AuthenticationPrincipal UserDetailsImpl userInfo, @RequestPart MultipartFile img){
+
+        User user = userService.getUserByUserId(userInfo.getId());
+
+        String personal = recommendService.insertPersonal(user, img);
+
+        return ResponseEntity.ok().body(personal);
+    }
+
+
+    @PostMapping("/personal/{style}")
+    public ResponseEntity<?> PersonalColorRecommend(@AuthenticationPrincipal UserDetailsImpl userInfo, @PathVariable String style){
+        User user = userService.getUserByUserId(userInfo.getId());
+        HashMap<Object, Object> result = recommendService.recommendPersonal(user.getPersonal_color(), "fall", user.getGender(), style);
+        return ResponseEntity.ok().body(result);
+    }
+
 }
