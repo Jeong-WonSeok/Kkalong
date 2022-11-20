3a5443a96544bf1f1b498d38618d14a3347cad99
tmddn1008 <tmddn1008@naver.com>
2022-11-21 04:43:27 +0900

    feat:three.js수정
    Former-commit-id: 685540f245601d27bd57504c998bab374d9f0703

--- kkalong_fe/src/components/closet/Example.js
+++ kkalong_fe/src/components/closet/Example.js
@@ -3,15 +3,19 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import styled from "styled-components";
 import Model from "./Model";
+
 import { useNavigate } from "react-router-dom";
 
 const Example = () => {
 
 
+
+
   const navigate = useNavigate();
   setTimeout(() => {
-    navigate('/VirtualFitting/VirtualBrandChoice/')
-  }, 4500)
+    // navigate('/VirtualFitting/VirtualBrandChoice/')
+    
+  }, 7500)
   
   // const updateHandler = (e) => {
   //   // console.log("업뎃핸들러")
@@ -32,43 +36,24 @@ const Example = () => {
   return (
     <>
       <Contain>
-        <Canvas>
+        <Canvas >
           <Suspense fallback={null}>
-           
-            <directionalLight intensity={1}
-              position={[0, -3, 5]}
+            <pointLight
+              intensity={0.3}
+              position={[0,7,-1]}
             />
-            <ambientLight intensity={0.9639} 
-              position={[0, -3, 3]}
+            <pointLight
+              intensity={1.2}
+              position={[7,8,4]}
             />
-            <PerspectiveCamera 
-            // <PerspectiveCamera onUpdate={updateHandler}
-
-                        makeDefault={true}
-                        far={300}
-                        near={0.1}
-                        fov={70.1}
-                        position={[0,0,6]}
-                        zoom={1}
-                        
-                    />
-            <spotLight
-              intensity={0.1}
-              angle={0.1}
-              penumbra={1}
-              position={[10, 15, 10]}
-              castShadow
+            <Model
             />
-            <Model 
-              position={[0, -3, -1]}
-              rotation-y={Math.PI * 1.5}
-              color="hotpink"
-            />
+{/* 
             <OrbitControls
               enablePan={true}
               enableZoom={true}
               enableRotate={true}
-            />
+            /> */}
           </Suspense>
         </Canvas>
       </Contain>
--- kkalong_fe/src/pages/Closet/ThreeTest.js
+++ kkalong_fe/src/pages/Closet/ThreeTest.js
@@ -1,12 +1,122 @@
 import Example from "../../components/closet/Example";
+import styled, { keyframes } from 'styled-components'
+import BackArrow from "../../assets/icon/Login/arrow.png"
+import { useNavigate } from "react-router-dom";
 
 const ThreeTest = () => {
+
+  const navigate = useNavigate();
+
+  const SkipHandler = () => {
+    navigate("/")
+  }
+
   return (
     <>
-      <h1>This is Three.js test page</h1>
+
+      <Bouncediv>
+        <div>
+          <Bounce>가</Bounce>
+          <Bounce>상</Bounce>
+          <Bounce>피</Bounce>
+          <Bounce>팅</Bounce>
+          <Bounce> </Bounce>
+          <Bounce>입</Bounce>
+          <Bounce>장</Bounce>
+          <Bounce>중</Bounce>
+          <Bounce>.</Bounce>
+          <Bounce>.</Bounce>
+        </div>
+        <SkipDiv onClick={SkipHandler}>
+        <SkipButton>바로 입장하기</SkipButton>
+        <BackArrowImg src={BackArrow}></BackArrowImg>
+        </SkipDiv>
+      </Bouncediv>
       <Example />
     </>
   );
 };
 
 export default ThreeTest;
+
+const Bouncediv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  
+`
+
+const BounceEffect = keyframes`
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
+
+
+
+const Bounce = styled.span`
+font-family: var(--base-font-300);
+
+&:nth-child(1) {
+ animation: ${BounceEffect} 2s linear infinite
+}
+&:nth-child(2) {
+ animation: ${BounceEffect} 2s linear infinite;
+ animation-delay: 0.1s;
+
+}
+&:nth-child(3) {
+ animation: ${BounceEffect} 2s linear infinite;
+ animation-delay: 0.2s;
+}
+&:nth-child(4) {
+ animation: ${BounceEffect} 2s linear infinite;
+ animation-delay: 0.3s;
+}
+&:nth-child(5) {
+ animation: ${BounceEffect} 2s linear infinite;
+ animation-delay: 0.4s;
+}
+&:nth-child(6) {
+ animation: ${BounceEffect} 2s linear infinite;
+ animation-delay: 0.5s;
+}
+&:nth-child(7) {
+ animation: ${BounceEffect} 2s linear infinite;
+ animation-delay: 0.6s;
+}
+&:nth-child(8) {
+ animation: ${BounceEffect} 2s linear infinite;
+ animation-delay: 0.7s;
+}
+&:nth-child(9) {
+ animation: ${BounceEffect} 2s linear infinite;
+ animation-delay: 0.8s;
+}
+&:nth-child(10) {
+ animation: ${BounceEffect} 2s linear infinite;
+ animation-delay: 0.9s;
+}
+ `
+
+const SkipButton = styled.button`
+  border : none;
+  border-radius: 2px;
+  background-color: #FFFFFF;
+  font-family: var(--base-font-300);  
+
+`
+
+const BackArrowImg = styled.img`
+  width: 25px;
+  height: 25px;
+`
+
+const SkipDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+
+`
